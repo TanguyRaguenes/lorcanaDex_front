fc3192016c23cae3b782f16506a44f80445c3f0d
Tanguy <tanguy.raguenes@gmail.com>
2024-10-04 21:00:33 +0200

    navbar + home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { CardsComponent } from './components/cards/cards.component';
 import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
 
-    {path: 'cards', component: CardsComponent},
-    {path: 'card/:id', component: CardComponent},
+    { path: 'home', component: HomeComponent },
+    { path: 'cards', component: CardsComponent },
+    { path: 'card/:id', component: CardComponent },
 
-    
+
 ];
