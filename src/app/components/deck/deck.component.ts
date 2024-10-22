b09f800db90a758ee5112e244d0757cbb8f5a5f7
Tanguy <tanguy.raguenes@gmail.com>
2024-10-22 20:47:51 +0200

    ajout et suppression cartes decks

--- src/app/components/deck/deck.component.ts
+++ src/app/components/deck/deck.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Deck } from '../../models/Deck';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { CardsComponent } from '../cards/cards.component';
+import { Card } from '../../models/Card';
 
 @Component({
   selector: 'app-deck',
@@ -13,7 +14,11 @@ import { CardsComponent } from '../cards/cards.component';
 })
 export class DeckComponent {
 
+  @ViewChild(CardsComponent) cardsComponent!: CardsComponent;
+
   protected deckSelected: Deck | null = null;
+  protected deckCards: Map<String, number> = new Map();
+  protected deckInks: Array<string | undefined> = [];
 
   constructor(private router: Router) {
     const data = sessionStorage.getItem("deckSelected");
@@ -30,12 +35,62 @@ export class DeckComponent {
         dataParse.secondInk)
     }
 
+    // this.deckInks = [this.deckSelected?.getFirstInk(), this.deckSelected?.getSecondInk()];
+    // this.cardsComponent.cardsStorage = this.cardsComponent.cardsStorage.filter(e => this.deckInks.includes(e.getColor() + ".png"))
 
+  }
+
+  ngAfterViewInit() {
+    this.deckInks = [this.deckSelected?.getFirstInk(), this.deckSelected?.getSecondInk()];
+
+    if (this.cardsComponent.cardsStorage) {
+      this.cardsComponent.cardsStorage = this.cardsComponent.cardsStorage.filter(e => this.deckInks.includes(e.getColor() + ".png"));
+    }
   }
 
 
   public goBack() {
     this.router.navigate(["/decks"])
+  }
+
+
+  public addCardToDeck(uniqueId: string): void {
+
+    const cardFiltered: Card = this.cardsComponent.cardsStorage.filter(e => e.getUniqueId() == uniqueId)[0]
+
+
+    let numberOfCopies: number = this.deckCards.get(uniqueId) || 0;
+
+
+    if (numberOfCopies < 4 && this.deckInks.includes(cardFiltered.getColor() + ".png")) {
+      console.log("ajout")
+      this.deckCards.set(uniqueId, numberOfCopies + 1);
+    } else {
+      console.log("pas d'ajout")
+    }
+
+    console.log(this.deckCards);
+
+  }
+
+  public removeCardFromDeck(uniqueId: string): void {
+
+    let numberOfCopies: number = this.deckCards.get(uniqueId) || 0;
+
+    if (numberOfCopies > 1) {
+      console.log("retranche")
+      this.deckCards.set(uniqueId, numberOfCopies - 1);
+    } else {
+      this.deckCards.delete(uniqueId);
+      console.log("suppression")
+    }
+
+    console.log(this.deckCards);
+
+  }
+
+  public getCardNumberOfCopies(uniqueId: string): number {
+    return this.deckCards.get(uniqueId) || 0;
   }
 
 
