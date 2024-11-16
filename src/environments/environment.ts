3ce9923f9be8b2ac17eb1c5680a1c8aed6cc5582
Tanguy <tanguy.raguenes@gmail.com>
2024-11-16 21:05:00 +0100

    test2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: true,
     apiGetCardsFromBDD: 'https://lorcanadexback-production.up.railway.app/get-cards',
     apiBulkCards: 'https://lorcanadexback-production.up.railway.app/bulk-data',
-    serverSide_decksApiRest: 'https://lorcanadexback-production.up.railway.app/decks',
+    serverSide_decksApiRest: 'http://lorcanadexback-production.up.railway.app/decks',
     serverSide_deckApiRest: 'https://lorcanadexback-production.up.railway.app/deck',
     serveSide_authApiRest: 'https://lorcanadexback-production.up.railway.app/auth',
     serveSide_registrationApiRest: 'https://lorcanadexback-production.up.railway.app/registration'
