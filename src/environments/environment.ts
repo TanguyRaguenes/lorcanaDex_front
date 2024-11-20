2e21d903d29edc2df49fdb03241c564ec6001e8e
TanguyRaguenes <tanguy.raguenes@gmail.com>
2024-11-20 12:15:35 +0100

    Nouvelle api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,12 @@ export const environment = {
     production: true,
     apiGetCardsFromBDD: 'https://lorcanadexback-production.up.railway.app/cards/get-cards',
     apiBulkCards: 'https://lorcanadexback-production.up.railway.app/cards/bulk-data',
+
+
+
+
+
+    serverside_cardsApiRest: 'http://lorcanadexback-production.up.railway.app/cards',
     serverSide_decksApiRest: 'https://lorcanadexback-production.up.railway.app/decks',
     serverSide_deckApiRest: 'https://lorcanadexback-production.up.railway.app/deck',
     serveSide_authApiRest: 'https://lorcanadexback-production.up.railway.app/auth',
